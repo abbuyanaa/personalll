3563412fee4d168c155358928f0e6eaad0e7d2f2
AB <abbuyanaa@gmail.com>
2026-09-21 11:51:16 +0900

    First commit

---
+++ store/Providers.js
@@ -0,0 +1,22 @@
+'use client';
+
+import React, { useRef } from 'react';
+import { Provider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
+import createStore from './configureStore';
+
+export default function Providers({ children, preloadedState }) {
+  const storeRef = useRef();
+
+  if (!storeRef.current) {
+    storeRef.current = createStore(preloadedState);
+  }
+
+  return (
+    <Provider store={storeRef.current}>
+      <CookiesProvider defaultSetOptions={{ path: '/', sameSite: 'lax' }}>
+        {children}
+      </CookiesProvider>
+    </Provider>
+  );
+}
---
+++ store/configureStore.js
@@ -0,0 +1,41 @@
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '@/reducers';
+import rootSaga from '@/sagas';
+import { isDev, isServer } from '@/config';
+
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === 'function') {
+    // console.log('[Logger] thunk-like: ', action);
+    return action(dispatch, getState);
+  }
+  // console.log('[Logger] dispatch: ', action);
+  return next(action);
+};
+
+const createStore = (preloadedState = {}) => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const middlewareEnhancer = (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: false,
+    serializableCheck: {
+      ignoredActionPaths: ['payload.data'],
+    },
+  })
+    .concat(
+      ...(isDev ? [loggerMiddleware] : []),
+      sagaMiddleware,
+    );
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: middlewareEnhancer,
+    devTools: !isServer && isDev,
+    preloadedState,
+  });
+
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+};
+
+export default createStore;
